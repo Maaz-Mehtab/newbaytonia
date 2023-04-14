f81ecbe8ab88990db6ed4a8e3d456904eebc1161
Maaz Mehtab <maazmehtabuddin@folio3.com>
2023-04-15 02:13:51 +0500

    fixes

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -3,5 +3,22 @@
 // export const BaseURL = "https://dev.oddo.baytonia.com";
 // live url
 // export const BaseURL = 'https://stage.oddo.baytonia.com';
-export const BaseURL = 'https://oddo.baytonia.com';
+// export const BaseURL = 'https://oddo.baytonia.com';
+export const BaseURL = 'http://164.90.222.22:8069';
+// export const BASIC_AUTH_KEY = 'Basic ZHVtbXlTZWNyZXRLZXk6ZHVtbXlTZWNyZXRLZXk=';
 export const BASIC_AUTH_KEY = 'Basic ZHVtbXlTZWNyZXRLZXk6ZHVtbXlTZWNyZXRLZXk=';
+
+
+// live user:
+// email:nuha@baytonia.com
+// password:'N123456789#'
+
+
+// live url: http://164.90.222.22:8069
+// email:karam@baytonia.com
+// password:'admin#'
+// email:nuha@baytonia.com
+// password:'N123456789!'
+
+
+
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,144 +1,139 @@
 import * as React from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native';
+import {View, Text, SafeAreaView, TouchableOpacity, Image} from 'react-native';
 import MainTextInput from '../../components/MainTextInput';
-import { Images } from "../../themes/Images";
-import Icon from "../../helpers/Icons";
-import styles from "./styles";
+import {Images} from '../../themes/Images';
+import Icon from '../../helpers/Icons';
+import styles from './styles';
 import Button from '../../components/Button';
 import LinearGradient from 'react-native-linear-gradient';
 import SplashScreen from 'react-native-splash-screen';
-import messaging from "@react-native-firebase/messaging";
+import messaging from '@react-native-firebase/messaging';
 import Buffer from 'buffer';
 import userAction from '../../store/action/user';
-import { useDispatch } from "react-redux";
+import {useDispatch} from 'react-redux';
 import Toast from 'react-native-toast-message';
-import util from '../../helpers/util'
-function Login({ navigation }) {
-    const dispatch = useDispatch();
-    // const [state, setState] = React.useState({ email: 'db@baytonia.com', password: 'Db123456789!!' })
-    // const [state, setState] = React.useState({ email: 'app.dev@baytonia.com', password: 'M123456!' })
-    // const [state, setState] = React.useState({ email: 'maaz.app@baytonia.com', password: 'M123!' })
-    const [state, setState] = React.useState({ email: '', password: '' })
-    const [loader,setLoader] = React.useState(false)
-    const _handleTextChange = (name, val) => {
-        setState({
-            ...state,
-            [name]: val
-        })
+import util from '../../helpers/util';
+function Login({navigation}) {
+  const dispatch = useDispatch();
+  // const [state, setState] = React.useState({ email: 'db@baytonia.com', password: 'Db123456789!!' })
+  // const [state, setState] = React.useState({ email: 'app.dev@baytonia.com', password: 'M123456!' })
+  // const [state, setState] = React.useState({ email: 'maaz.app@baytonia.com', password: 'M123!' })
+  const [state, setState] = React.useState({email: '', password: ''});
+  const [loader, setLoader] = React.useState(false);
+  const _handleTextChange = (name, val) => {
+    setState({
+      ...state,
+      [name]: val,
+    });
+  };
+
+  SplashScreen.hide();
+
+  const fetchFCMToken = async () => {
+    const fcmToken = await messaging().getToken();
+    return fcmToken;
+  };
+
+  const loginUserCheck = async () => {
+    try {
+      if (state.email == '') {
+        util.errorMsg('Enter Email Address');
+        return;
+      } else if (state.password == '') {
+        util.errorMsg('Enter Password');
+        return;
+      }
+      await login();
+    } catch (error) {
+      console.log('exception', error);
     }
+  };
 
-    SplashScreen.hide()
-
-  
-    const fetchFCMToken = async () => {
-        const fcmToken = await messaging().getToken()
-        return fcmToken
-    }
-
-   const  loginUserCheck = async ()=>{
-        try {
-
-            if (state.email == '') {
-              util.errorMsg('Enter Email Address');
-              return;
-            } else if (state.password == '') {
-              util.errorMsg('Enter Password');
-              return;
-            }
-            await login();  
-        } catch (error) {
-            console.log("exception",error)
+  const login = async () => {
+    try {
+      setLoader(true);
+      let fcmToken = await fetchFCMToken();
+      let loginrequestObject = {
+        login: state.email,
+        pwd: state.password,
+      };
+      let loginRequestString = JSON.stringify(loginrequestObject);
+      let loginRequestBase64 =
+        Buffer.Buffer.from(loginRequestString).toString('base64');
+      let requestbody = {
+        fcmToken: fcmToken,
+        fcmDeviceId: 'DoctorStrange',
+        login: loginRequestBase64,
+      };
+      dispatch(userAction.login(requestbody)).then(res => {
+        if (res?.success) {
+          util.successMsg(res?.message);
+          navigation.navigate('Drawer');
+          setLoader(false);
+        } else {
+          util.errorMsg(res?.message);
+          setLoader(false);
         }
+      });
+    } catch (e) {
+      setLoader(false);
+      console.log('Exception => login', e);
     }
+  };
 
-    const login = async () => {
-        try {
-            setLoader(true)
-            let fcmToken = await fetchFCMToken();
-            let loginrequestObject = {
-                "login": state.email,
-                "pwd": state.password
-            }
-            let loginRequestString = JSON.stringify(loginrequestObject)
-            let loginRequestBase64 = Buffer.Buffer.from(loginRequestString).toString("base64")
-            let requestbody = {
-                "fcmToken": fcmToken,
-                "fcmDeviceId": "DoctorStrange",
-                "login": loginRequestBase64
-            }
-            dispatch(userAction.login(requestbody)).then(res => {
-                if (res?.success) {
-                    util.successMsg(res?.message)
-                    navigation.navigate("Drawer")
-                    setLoader(false)
-                }
-                else {
-                    util.errorMsg(res?.message)
-                    setLoader(false)
-                }
-            })
-        } catch (e) {
-            setLoader(false)
-            console.log("Exception => login", e);
+  return (
+    <LinearGradient colors={['#f2f2f2', '#f2f2f2']} style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.logoView}>
+          <Image style={styles.logo} source={Images.logo} />
+        </View>
 
-        }
-    }
+        <MainTextInput
+          Icon={
+            <Icon.MaterialCommunityIcons
+              name="email-outline"
+              style={styles.iconStyle}
+            />
+          }
+          onChangeText={t => _handleTextChange('email', t)}
+          value={state.email}
+          label={'Email'}
+          placeholder=""
+          keyboardType={'email-address'}
+          autoCapitalize={'none'}
+        />
 
-    return (
-        <LinearGradient colors={['#f2f2f2', '#f2f2f2']} style={styles.container}>
-            <SafeAreaView style={styles.container}>
+        <MainTextInput
+          Icon={
+            <Icon.MaterialCommunityIcons
+              name="lock-outline"
+              style={styles.iconStyle}
+            />
+          }
+          secureTextEntry={true}
+          onChangeText={t => _handleTextChange('password', t)}
+          value={state.password}
+          label={'Password'}
+          // placeholder="**********"
+          autoCapitalize={'none'}
+          rightIcon={true}
+          passowrdhide={true}
+        />
 
-                <View style={styles.logoView}>
-                    <Image style={styles.logo} source={Images.logo} />
-
-                </View>
-
-
-                <MainTextInput
-                    Icon={
-                        <Icon.MaterialCommunityIcons
-                            name="email-outline"
-                            style={styles.iconStyle}
-                        />
-                    }
-                    onChangeText={(t) => _handleTextChange("email", t)}
-                    value={state.email}
-                    label={"Email"}
-                    placeholder=""
-                    keyboardType={"email-address"}
-                    autoCapitalize={"none"}
-                />
-
-
-                <MainTextInput
-                    Icon={
-                        <Icon.MaterialCommunityIcons
-                            name="lock-outline"
-                            style={styles.iconStyle}
-                        />
-                    }
-                    secureTextEntry={true}
-                    onChangeText={(t) => _handleTextChange("password", t)}
-                    value={state.password}
-                    label={"Password"}
-                    // placeholder="**********"
-                    autoCapitalize={"none"}
-                    rightIcon={true}
-                    passowrdhide={true}
-                />
-
-                <View style={styles.bottomContainer}>
-                    <View style={styles.buttonView}>
-                        <Button loader={loader} btnPress={loginUserCheck} label={"Login"} />
-                    </View>
-                    <TouchableOpacity onPress={()=>navigation.navigate("Signup")} style={styles.buttonView}>
-                        <Text style={styles.registerText}>Register a New User</Text>
-                    </TouchableOpacity>
-                </View>         
-                <Toast ref={(ref) => Toast.setRef(ref)} />
-            </SafeAreaView>
-        </LinearGradient>
-    );
+        <View style={styles.bottomContainer}>
+          <View style={styles.buttonView}>
+            <Button loader={loader} btnPress={loginUserCheck} label={'Login'} />
+          </View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Signup')}
+            style={styles.buttonView}>
+            <Text style={styles.registerText}>Register a New User</Text>
+          </TouchableOpacity>
+        </View>
+        <Toast ref={ref => Toast.setRef(ref)} />
+      </SafeAreaView>
+    </LinearGradient>
+  );
 }
 
 export default Login;
